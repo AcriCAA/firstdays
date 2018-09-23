5058827d4324b0a97b53f8c149b60e13b404bdf5
Corey Acri <acri.corey@gmail.com>
2018-09-23 08:17:03 -0400

    counts has tab and back button now

--- resources/views/posts/counts.blade.php
+++ resources/views/posts/counts.blade.php
@@ -1,29 +1,44 @@
 @extends('layout')
 
 @section('content')
-	<h1>Counts</h1>
-<div class="row mt-3">
+
+      <div class="row mt-3">
+            <div class="col">
+
+              <p class="float-left"> <a class="btn btn-primary btn-sm" href="/">
+          back</i>
+        </a></p>
+            </div>
+          </div>
+	<h1 class="mt-2">Counts</h1>
+{{-- <div class="row mt-3"> --}}
 
 
- <div class="col px-3 border">
- 	<p class="text-center"><i class="fas fa-3x fa-oil-can"></i></p>
- 	@foreach($peecounts as $pee)
+
+
+
+<ul class="nav nav-tabs mt-3" id="myTab" role="tablist">
+  
+  <li class="nav-item">
+    <a class="nav-link active" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false"><i class="fas fa-3x fa-oil-can"></i><p class="small text-center">Pee</p></a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false"><i class="fas fa-3x fa-poop"></i><p class="small text-center">Poop</p></a>
+  </li>
+</ul>
+<div class="tab-content mt-2" id="myTabContent">
+
+  <div class="tab-pane fade show active" id="profile" role="tabpanel" aria-labelledby="profile-tab">@foreach($peecounts as $pee)
  	<h2>{{$pee->month}} {{$pee->day}}</h2>
  	<p>Pee: {{$pee->pee}}</p>
  	@endforeach
- 	
-  
-</div>
-
- <div class="col px-3 border">
- 	<p class="text-center"><i class="fas fa-3x fa-poop"></i></p>
- 	@foreach($poopcounts as $poop)
+ 	</div>
+  <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+  	@foreach($poopcounts as $poop)
  	<h2>{{$poop->month}} {{$poop->day}}</h2>
  	<p>Poop: {{$poop->poop}}</p>
  	@endforeach
- 	
-  
-</div>
+
 </div>
 
 @endsection
