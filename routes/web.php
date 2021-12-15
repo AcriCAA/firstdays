321a83b7112e9abdf412a50f7a019aa2cdc78719
Shift <shift@laravelshift.com>
2021-12-15 19:31:28 +0000

    Shift to class based routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,17 +21,17 @@
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/', 'PostController@index');
-Route::get('/create', 'PostController@create');
+Route::get('/', [PostController::class, 'index']);
+Route::get('/create', [PostController::class, 'create']);
 
-Route::get('/counts', 'PostController@counts');
+Route::get('/counts', [PostController::class, 'counts']);
 
-Route::post('/store', 'PostController@store');
+Route::post('/store', [PostController::class, 'store']);
 
-Route::get('/{post}', 'PostController@show');
+Route::get('/{post}', [PostController::class, 'show']);
 
-Route::patch('/{post}', 'PostController@update');
+Route::patch('/{post}', [PostController::class, 'update']);
 
-Route::get('/edit/{post}', 'PostController@edit');
+Route::get('/edit/{post}', [PostController::class, 'edit']);
