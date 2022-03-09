21d8ddc6dd73c0d74f8de007386dd84d3f8b3de4
Corey A <corey@agstrategic.design>
2022-03-09 07:46:33 -0500

    changing counts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -40,7 +40,7 @@ class PostController extends Controller
 			monthname(timelogged_timestamp) month, 
 			count(*) pee'
         )
-        ->groupBy('day', 'month')
+        ->groupBy('month')
         ->get();
 
         $poopcounts = Post::where('user_id', $id)
@@ -50,7 +50,7 @@ class PostController extends Controller
 			monthname(timelogged_timestamp) month, 
 			count(*) poop'
         )
-        ->groupBy('day', 'month')
+        ->groupBy('month')
         ->get();
 
         // echo '<pre>';
